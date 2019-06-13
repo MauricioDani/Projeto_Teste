c08c13c636a0ab6ee5d74098c9733a250e009089
MauricioDani <mauricio.cdani@gmail.com>
2019-06-13 13:21:27 -0300

    Evolução estudos
    até if statments

--- JS/Treinamento03_funcoes/script4.js
+++
@@ -1,9 +0,0 @@
-var number = prompt("Enter a number:",2);
-
-if(number > 1){
-    for(var i = 1; i<= number; i++){
-        if((number % i == 0) && number != 1 && number != i){
-            
-        }
-    }
-}
