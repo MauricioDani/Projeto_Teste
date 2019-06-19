060c743b073df7cca43e10eb913cb5e246e752a9
MauricioDani <mauricio.cdani@gmail.com>
2019-06-19 16:31:02 -0300

    Fim Objetos(básico)
    Inicio data types

--- JS/objects_methods/tasks.js
+++ JS/objects_methods/tasks.js
@@ -157,3 +157,62 @@ var cyl = new Cylinder(7, 4);
 // Volume of the cylinder with four decimal places.
 console.log('volume =', cyl.Volume().toFixed(4));
 
+/* -------------------------------------------------TASK 6----------------------------------------------------------
+Create a constructor function Calculator that creates objects with 3 methods:
+
+    read() asks for two values using prompt and remembers them in object properties.
+    sum() returns the sum of these properties.
+    mul() returns the multiplication product of these properties.*/
+
+let newCalculator = new Calculator();
+
+newCalculator.read();
+alert( newCalculator.sum() );
+alert( newCalculator.mul() );
+
+function Calculator() {
+  this.read = function() {
+      this.x = +prompt("x",0);
+      this.y = +prompt("y",0);
+  };
+
+  this.sum = function() {
+    return this.x + this.y ;
+  };
+
+  this.mul = function() {
+    return this.x * this.y;
+  };
+}
+
+/* -------------------------------------------------TASK 7----------------------------------------------------------
+Create a constructor function Accumulator(startingValue).
+
+Object that it creates should:
+
+    Store the “current value” in the property value. The starting value is set to the argument of the constructor startingValue.
+    The read() method should use prompt to read a new number and add it to value.
+
+In other words, the value property is the sum of all user-entered values with the initial value startingValue.
+
+Here’s the demo of the code:
+
+let accumulator = new Accumulator(1); // initial value 1
+accumulator.read(); // adds the user-entered value
+accumulator.read(); // adds the user-entered value
+alert(accumulator.value); // shows the sum of these values
+
+*/
+
+let accumulator = new Accumulator(1);
+
+accumulator.read();
+accumulator.read();
+alert(accumulator.value);
+
+function Accumulator(startingValue) {
+  this.value = startingValue;
+  this.read = function() {
+    this.value += +prompt("Valor:",0)
+  }
+}
