1d8e30b956d619b479fbe0dc8db220ec8dad1a88
MauricioDani <mauricio.cdani@gmail.com>
2019-07-01 09:19:09 -0300

    Update array_tasks.js
    Final Array Inicio Iterables

--- JS/data_type/array_tasks.js
+++ JS/data_type/array_tasks.js
@@ -237,14 +237,6 @@ let usersMapped = users.map(user => ({
     id: user.id
 }));
 
-/*
-usersMapped = [
-  { fullName: "John Smith", id: 1 },
-  { fullName: "Pete Hunt", id: 2 },
-  { fullName: "Mary Key", id: 3 }
-]
-*/
-
 alert(usersMapped[0].id) // 1
 alert(usersMapped[0].fullName) // John Smith
 
